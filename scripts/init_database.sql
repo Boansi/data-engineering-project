b9f1690c93781b0f6573bb79376828caf57c6f30
Emmanuel Boansi Wiredu <109662487+Boansi@users.noreply.github.com>
2025-11-04 15:58:21 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,25 @@
+/*
+The purpose for the script is to build the various schemas for the database; bronze, silver and gold.
+This is for the backup incase the orginal one does not exist
+*/
 
+-- Creating a datawarehouse
+
+USE master;
+
+--creating a database
+Create database DatawareHouse;
+
+use DatawareHouse;
+
+-creating the bronze 
+CREATE SCHEMA Bronze;
+GO
+
+ -creating the silver 
+CREATE SCHEMA Silver;
+GO
+
+-creating the gold
+CREATE SCHEMA Gold;
+GO
